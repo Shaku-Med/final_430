7a0d74fea5167f5609cc885b75b60ba813eadb47
Shaku-Med <jujubelt124@gmail.com>
2025-05-11 21:44:02 -0400

    s

--- FileUploader/src/app.js
+++ FileUploader/src/app.js
@@ -61,7 +61,7 @@ const securityHeaders = (req, res, next) => {
     res.setHeader('X-Content-Type-Options', 'nosniff');
     res.setHeader('X-XSS-Protection', '1; mode=block');
     next();
-};
+}
 
 app.use(bodyParser.json({
     limit: '200mb'
